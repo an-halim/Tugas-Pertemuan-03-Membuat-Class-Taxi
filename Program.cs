30746f6669509cba083244a24cdf7d9955a5b499
Ahmad nur halim <halimbla2@gmai.com>
2021-03-16 17:37:15 +0700

    Tugas membuat class taxi

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Tugas Object dan Class";
+            Taxi taksi = new Taxi();
+            taksi.Nama = "Ahmad Nur Halim";
+            taksi.Ready = true;
+            taksi.Passenger = 10;
+
+            taksi.info();
+            taksi.pickUp();
+            taksi.dropDown();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Taxi
+    {
+        public string Nama { get; set; }
+        public int Passenger { get; set; }
+        public bool Ready { get; set; }
+        public void info()
+        {
+            Console.WriteLine("Driver Name: {0}", Nama);
+            if(Ready == true)
+                Console.WriteLine("On Duty: Yes");
+            else
+                Console.WriteLine("On Duty: No");
+            Console.WriteLine("Number Of Passenger: {0}\n", Passenger);
+        }
+
+        public void pickUp()
+        {
+            Console.WriteLine("{0} Sedang menjemput {1} penumpang!", Nama, Passenger);
+        }
+        public void dropDown()
+        {
+            Console.WriteLine("{0} Selesai mengantarkan {1} penumpang!", Nama, Passenger);
+        }
+    }
+}
